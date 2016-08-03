3eff07a4bdfe806bb279f9b733258c56856b2997
vagrant <vagrant@homestead>
2016-08-03 06:19:53 +0000

    modify run.sh

--- run.sh
+++ run.sh
@@ -3,20 +3,20 @@ BACKUP_DIR=~/.backup
 if [ -e ~/.vim ]; then
 	mkdir -p ${BACKUP_DIR}
 	mv ~/.vim ${BACKUP_DIR}/.vim
-	ln -s ${DIR}/.vim ~/.vim
 fi
+ln -s ${DIR}/.vim ~/.vim
 if [ -e ~/.vimrc ]; then
 	mkdir -p ${BACKUP_DIR}
 	mv ~/.vimrc ${BACKUP_DIR}/.vimrc
-	ln -s ${DIR}/.vimrc ~/.vimrc
 fi
+ln -s ${DIR}/.vimrc ~/.vimrc
 if [ -e ~/.zsh ]; then
 	mkdir -p ${BACKUP_DIR}
 	mv ~/.zsh ${BACKUP_DIR}/.zsh
-	ln -s ${DIR}/.zsh ~/.zsh
 fi
+ln -s ${DIR}/.zsh ~/.zsh
 if [ -e ~/.zshrc ]; then
 	mkdir -p ${BACKUP_DIR}
-	ln -s ${DIR}/.zshrc ${BACKUP_DIR}/.zshrc
 fi
+ln -s ${DIR}/.zshrc ${BACKUP_DIR}/.zshrc
 
